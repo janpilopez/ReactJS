f91c669a3cb35841b75d355955427a9fcf3f7349
Jean <jean1xd@gmail.com>
2025-03-22 23:11:55 -0400

    sumador fin

--- sumador/src/Components/ContentRating.jsx
+++ sumador/src/Components/ContentRating.jsx
@@ -5,11 +5,41 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    //OBJETO ESTADO INICIALIZANDO LOS VALORES
+    this.state = {
+        likes: 0,
+        dislikes: 0,
+        //manejadores de eventos 
+        handleLike:() => {
+            this.setState( (prevState) => ({
+                likes: parseInt(prevState.likes)+1
+            } ));
+        },
+        handleDislike:() => {
+            this.setState( prevState => ({
+                dislikes: parseInt(prevState.dislikes)+1
+            }));
+        }
+    };
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+        <p>
+            Hola mucho gusto,
+            esto es una prueba si te gusta
+            like si no dislike xd
+        </p>
+        <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike} >
+                Like ( {this.state.likes} )
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+                Dislike ( {this.state.dislikes} )
+            </button> 
+        </div>
+     </div>
      </>
     );
   }
